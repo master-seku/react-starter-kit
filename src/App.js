f88e9036f0a889b460a07399b7b0d82dbe4d2725
Ahmet Efe Korkmaz <sekullarx@gmail.com>
2024-06-27 16:00:23 +0300

    İlk react proje kodları

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import logo2 from "./yone.jpg"
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Adeta bir seks makinasiyim</h1>
+      <img src={logo2}></img>
+    </div>
+  );
+}
+
+export default App;
